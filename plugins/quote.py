42efe5ec34df7f3aee4fa3c1a50f643f70b81f33
Jani Willman <jani.willman@gmail.com>
2019-08-13 19:56:57 +0300

    Create quote.py

---
+++ plugins/quote.py
@@ -0,0 +1,12 @@
+import os.path
+import saxo
+
+@saxo.setup
+def setup(irc):
+    path = os.path.join(irc.base, "database.sqlite3")
+    with saxo.database(path) as db:
+        if "saxo_quote" not in db:
+            db["saxo_quote"].create(
+                ("unixtime", int),
+                ("channel", str),
+                ("message", str))
