ed62d9f0059e28b52a7539e15d83df129ce09b00
Jani Willman <jani.willman@gmail.com>
2019-08-24 22:55:45 +0300

    Update links.py

--- plugins/links.py
+++ plugins/links.py
@@ -6,9 +6,17 @@ import saxo
 
 regex_link = re.compile(r"(http[s]?://[^<> \"\x01]+)[,.]?")
 
+def everything_between(text,begin,end):
+    idx1=content.find(begin)
+    idx2=content.find(end,idx1)
+    return content[idx1+len(begin):idx2].strip()
+
 @saxo.event("PRIVMSG")
 def link(irc):
     search = regex_link.search(irc.text)
     if search:
         if irc.sender.startswith("#"):
             irc.client("link", irc.sender, search.group(1))
+            content = open(search.group(1)).read()
+            title = everything_between(content,'<title>','</title>')
+            irc.say(title)
