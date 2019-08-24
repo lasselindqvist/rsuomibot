5e3ed52bcec99ba468ad0c7cf6516b86cf5f9213
Jani Willman <jani.willman@gmail.com>
2019-08-24 23:04:48 +0300

    Update links.py

--- plugins/links.py
+++ plugins/links.py
@@ -3,15 +3,8 @@
 
 import re
 import saxo
-import urllib2
-from BeautifulSoup import BeatifulSoup
 
 regex_link = re.compile(r"(http[s]?://[^<> \"\x01]+)[,.]?")
-
-def everything_between(text,begin,end):
-    idx1=content.find(begin)
-    idx2=content.find(end,idx1)
-    return content[idx1+len(begin):idx2].strip()
 
 @saxo.event("PRIVMSG")
 def link(irc):
@@ -19,7 +12,3 @@ def link(irc):
     if search:
         if irc.sender.startswith("#"):
             irc.client("link", irc.sender, search.group(1))
-            URLObject = urllib2.urlopen(search.group(1))
-            html = BeautifulSoup(URLObject.read())
-            title = html.find('title')
-            irc.say(title)
